e492f7e885fd96b5cbb1e648e2a053ebc4bbe1f9
mihaialu <mhotineanu@gmail.com>
2024-10-15 14:45:36 +0300

    Added the Variables and Comments task

---
+++ src/main/java/org/example/variableandcomments/VariablesAndComents.java
@@ -0,0 +1,33 @@
+package org.example.variableandcomments;
+
+public class VariablesAndComents {
+
+    public static void main(String[] args) {
+
+        //Declare section
+
+        boolean exampleOfDooLeanVariable = true;
+        char exampleOfCharVariable;
+        int exampleOfIntVariable;
+        byte exampleOfByteVariable;
+        short exampleOfShortVariable;
+        long exampleOfLongVariable;
+        float exampleOfFloatVariable;
+        double exampleOfDoubleVariable;
+
+        // Initialization section
+
+        exampleOfCharVariable = 'C';
+        exampleOfIntVariable = 5894589;
+        exampleOfByteVariable = 12;
+        exampleOfShortVariable = 32000;
+        exampleOfLongVariable = 895658778;
+        exampleOfFloatVariable = 67.3f;
+        exampleOfDoubleVariable = 357.5626874569;
+
+        System.out.println(exampleOfShortVariable);
+
+        // CTRL + ALT + L - pretty code
+    }
+
+}
