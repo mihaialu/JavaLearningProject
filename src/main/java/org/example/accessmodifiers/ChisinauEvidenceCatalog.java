3876353de5ddcd46079e84c3215faf28012f9c88
mihaialu <mhotineanu@gmail.com>
2024-10-22 15:39:04 +0300

    Added the deatails for the Java Variables and Modifiers task.

---
+++ src/main/java/org/example/accessmodifiers/ChisinauEvidenceCatalog.java
@@ -0,0 +1,36 @@
+package org.example.accessmodifiers;
+
+public class ChisinauEvidenceCatalog {
+    public static void main(String[] args) {
+        1 = new Person(firstName:"Ion", lastName:"Busuioc", id:546245){
+            System.out.println(ion001.ID);
+            System.out.println(ion001.getAge());
+
+            01 = new Person(firstName:"Olga", lastName:"Grozavu", id:5646546, age:18, isRetired:false);
+            System.out.println(olga001.getAge());
+
+            n = new person = new Person("Valoare", "Valoare", id:546465, age:524, isRetired = true);
+            System.out.println(person.getAge());
+
+
+            if (ion 001.isRetired){
+                System.out.println("ion001.firstName + "este pensionat");
+            } else {
+                System.out.println("ion001.firstName + "nu este pensionat");
+            }
+
+            if (ion 001.isRetired){
+                System.out.println("olga001.firstName + "este pensionat");
+            } else {
+                System.out.println("olga001.firstName + "nu este pensionat");
+            }
+            ion001.printRetirementStatus();
+
+            olga001.printRetirementStatus();
+
+            person.printretiRementStatus();
+
+            Person.isHuman = false;
+        }
+    }
+}
